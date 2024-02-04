c3a50e1a2e3ced5fd11316645894c5cd090a3e7d
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-04 12:02:09 -0300

    adding the classes to specific file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -58,36 +58,3 @@ fun main() {
 }
 
 
-data class PersonClothers( var clothes: List<Clothe>, val  amount :Int = 3 )
-
-data class Clothe( var name :String,  val size :Char,  var color: java.awt.Color? )  // automatically generated methods:  equals(), hashCode(), toString() and copy()
-
-
-open class Person{
-
-        private lateinit var isWorking :BooleanArray
-
-        private val name: String
-
-        private  var age: Any?
-
-        constructor( nameReceived :String, ageReceived :Any?  ){
-                 this.name = nameReceived
-                 this.age = ageReceived
-        }
-
-
-        fun getName(  anywayValue : Any? = 4 ) : String{
-                 return this.name
-       }
-}
-
-
-class Teste : Person, Vehicles.Car {
-
-    constructor( nam: String, ag: Any? ) : super(nam, ag)
-
-    override fun goToBacking() {
-            println(" goToBacking on Teste class...")
-    }
-}
