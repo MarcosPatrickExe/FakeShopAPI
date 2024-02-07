7bd9b35f1c512d739bc5172376be310d77ae44ea
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-06 23:58:22 -0300

    editing the more one example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -97,7 +97,7 @@ fun main() {
     val pokemons = HashMap<String, PokeTypes>();
     pokemons.put("Blastoise", PokeTypes.WATER )
     pokemons.put("Ursaring",  PokeTypes.NORMAL )
-    pokemons.put("Ursaring",  PokeTypes.DRAGON )
+    pokemons.put("Salamence",  PokeTypes.DRAGON )
 
     var pokes  = Pokemons( pokemons )
 
