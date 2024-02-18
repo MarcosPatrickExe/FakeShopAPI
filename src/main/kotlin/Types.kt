1dc79e2c1c5c0a7a4c8ca2c63100d61fda3a84c6
MarcosPatrickExe <marcospatrick039474@gmail.com>
2024-02-18 20:18:51 -0300

    adding enum classes with constructor

--- src/main/kotlin/Types.kt
+++ src/main/kotlin/Types.kt
@@ -13,6 +13,13 @@ enum class PokeTypes{
 }
 
 
+enum class Color( val rgb :Int){
+        RED(0xFF0000),
+        GREEN( 0x00FF00),
+        BLUE( 0x0000FF),
+        YELLOw( 0xFFFF00);
 
+        fun containsRed() = (this.rgb and 0xFF0000 != 0)
+}
 
 
